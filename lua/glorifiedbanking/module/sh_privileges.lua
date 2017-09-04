7597f34cd803f910181e1ed3c16b02d70b6d9689
Kevin <fruitwasp@users.noreply.github.com>
2017-09-04 21:26:10 +0200

    something with load order

--- lua/glorifiedbanking/module/sh_privileges.lua
+++
@@ -1,9 +0,0 @@
-
-glorifiedBanking.privilege = {
-    CAMI_CAN_USE_ADMIN_COMMANDS = "glorifiedBanking_hasAdminPrivileges"
-}
-
-CAMI.RegisterPrivilege {
-    Name = glorifiedBanking.privilege.CAMI_CAN_USE_ADMIN_COMMANDS,
-    MinAccess = glorifiedBanking.config.ADMIN_INHERIT_MINIMUM
-}
