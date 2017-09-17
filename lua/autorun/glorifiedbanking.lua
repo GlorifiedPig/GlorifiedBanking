7cedb43b1c55f9dd754e5fc627709f423771d335
Kevin <fruitwasp@users.noreply.github.com>
2017-09-17 17:29:05 +0200

    updated frile to version 2

--- lua/autorun/glorifiedbanking.lua
+++ lua/autorun/glorifiedbanking.lua
@@ -6,24 +6,24 @@ glorifiedBanking = glorifiedBanking or {
     NICE_NAME = "glorifiedBanking"
 }
 
-local version = 1
+local version = 2
 
 if not frile or frile.VERSION < version then
     frile = {
         VERSION = version,
 
-        STATE_SERVER = 0,
-        STATE_CLIENT = 1,
-        STATE_SHARED = 2
+        REALM_SERVER = 0,
+        REALM_CLIENT = 1,
+        REALM_SHARED = 2
     }
 
     function frile.includeFile( filename, state )
-        if state == frile.STATE_SHARED or filename:find( "sh_" ) then
+        if state == frile.REALM_SHARED or filename:find( "sh_" ) then
             if SERVER then AddCSLuaFile( filename ) end
             include( filename )
-        elseif state == frile.STATE_SERVER or SERVER and filename:find( "sv_" ) then
+        elseif state == frile.REALM_SERVER or SERVER and filename:find( "sv_" ) then
             include( filename )
-        elseif state == frile.STATE_CLIENT or filename:find( "cl_" ) then
+        elseif state == frile.REALM_CLIENT or filename:find( "cl_" ) then
             if SERVER then AddCSLuaFile( filename )
             else include( filename ) end
         end
