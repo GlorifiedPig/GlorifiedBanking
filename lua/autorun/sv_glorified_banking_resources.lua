7a151b54e94d252d37289e7daa3d5c19b5efd35d
GlorifiedPig <killingpigs123@gmail.com>
2018-07-13 15:05:12 +0200

    workshop models

--- lua/autorun/sv_glorified_banking_resources.lua
+++ lua/autorun/sv_glorified_banking_resources.lua
@@ -1,5 +1,3 @@
 if !SERVER then return end
 
-resource.AddFile( "materials/models/atm/Texture.vmt" )
-resource.AddFile( "materials/models/atm/Texture.vtf" )
-resource.AddFile( "models/atm.mdl" )
+resource.AddWorkshop( "1439845273" )
