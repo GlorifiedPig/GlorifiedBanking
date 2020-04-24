9dbe5769c19013c098303a687ec35e9af9906ddb
GlorifiedPig <killingpigs123@gmail.com>
2020-04-24 17:16:23 +0200

    almost finish main panel

--- lua/glorifiedbanking/modules/ui/cl_glorifiedbanking_panel.lua
+++ lua/glorifiedbanking/modules/ui/cl_glorifiedbanking_panel.lua
@@ -3,6 +3,12 @@ local function GBScaleUI( num )
     return num * ( ScrH() / 720 )
 end
 
+surface.CreateFont( "GBRoboto14", {
+    font = "Roboto",
+    size = GBScaleUI( 14 ),
+    weight = 400
+} )
+
 surface.CreateFont( "GBRoboto18", {
     font = "Roboto",
     size = GBScaleUI( 18 ),
@@ -15,11 +21,22 @@ surface.CreateFont( "GBRoboto22", {
     weight = 300
 } )
 
+surface.CreateFont( "GBRoboto38", {
+    font = "Roboto",
+    size = GBScaleUI( 38 ),
+    weight = 300
+} )
+
 surface.CreateFont( "GBRoboto18Bold", {
     font = "Roboto",
     size = GBScaleUI( 18 ),
     weight = 1000,
     bold = true
+} )
+
+surface.CreateFont( "GBRoboto48", {
+    font = "Roboto",
+    size = GBScaleUI( 48 )
 } )
 
 function GlorifiedBanking.OpenPanel()
@@ -28,7 +45,7 @@ function GlorifiedBanking.OpenPanel()
     bankingFrame:ClearPaint()
     bankingFrame:Background( GlorifiedBanking.Config.GRADIENT_ONE )
     bankingFrame:Gradient( GlorifiedBanking.Config.GRADIENT_TWO )
-    bankingFrame:SetSize( GBScaleUI( 400 ), GBScaleUI( 550 ) )
+    bankingFrame:SetSize( GBScaleUI( 400 ), GBScaleUI( 500 ) )
     bankingFrame:ShowCloseButton( false )
     bankingFrame:SetDraggable( false )
     bankingFrame:SetTitle( "" )
@@ -52,7 +69,9 @@ function GlorifiedBanking.OpenPanel()
     paintPanel:SetSize( bankingFrame:GetWide(), bankingFrame:GetTall() )
     paintPanel:ClearPaint()
     paintPanel.Paint = function( self, w, h )
-        draw.RoundedBox( 0, 0, 40, w, 1, Color( 255, 255, 255 ) )
+        draw.RoundedBox( 0, 0, 40, w, 1, Color( 200, 200, 200 ) )
+        draw.SimpleText( "ACTIONS", "GBRoboto14", 5, h / 2.24 )
+        draw.RoundedBox( 0, 0, h / 2.07, w - 37, 1, Color( 255, 255, 255 ) )
     end
 
     local closeButton = TDLib( "DButton", bankingFrame )
@@ -85,7 +104,51 @@ function GlorifiedBanking.OpenPanel()
     userAvatar:SetPlayer( LocalPlayer(), 128 )
     userAvatar:SetSize( 128, 128 )
     userAvatar:CenterHorizontal( 0.5 )
-    userAvatar:CenterVertical( 0.22 )
+    userAvatar:CenterVertical( 0.24 )
+
+    local balanceLabel = TDLib( "DLabel", bankingFrame )
+    balanceLabel:SetFont( "GBRoboto38" )
+    balanceLabel:SetText( "$1,000,000,000" )
+    balanceLabel:SetTextColor( Color( 255, 255, 255 ) )
+    balanceLabel:SizeToContents()
+    balanceLabel:CenterHorizontal( 0.5 )
+    balanceLabel:CenterVertical( 0.41 )
+
+    local withdrawButton = TDLib( "DButton", bankingFrame )
+    withdrawButton:ClearPaint()
+    withdrawButton:Background( Color( 255, 255, 255, 0 ) )
+    withdrawButton:Outline( Color( 255, 255, 255 ), 2 )
+    withdrawButton:CircleHover( Color( 255, 255, 255, 25 ) )
+    withdrawButton:SetText( "Withdraw" )
+    withdrawButton:SetFont( "GBRoboto48" )
+    withdrawButton:SetSize( GBScaleUI( bankingFrame:GetWide() - 72 ), GBScaleUI( 72 ) )
+    withdrawButton:SetTextColor( Color( 255, 255, 255 ) )
+    withdrawButton:CenterHorizontal()
+    withdrawButton:CenterVertical( 0.58 )
+
+    local depositButton = TDLib( "DButton", bankingFrame )
+    depositButton:ClearPaint()
+    depositButton:Background( Color( 255, 255, 255, 0 ) )
+    depositButton:Outline( Color( 255, 255, 255 ), 2 )
+    depositButton:CircleHover( Color( 255, 255, 255, 25 ) )
+    depositButton:SetText( "Deposit" )
+    depositButton:SetFont( "GBRoboto48" )
+    depositButton:SetSize( GBScaleUI( bankingFrame:GetWide() - 72 ), GBScaleUI( 72 ) )
+    depositButton:SetTextColor( Color( 255, 255, 255 ) )
+    depositButton:CenterHorizontal()
+    depositButton:CenterVertical( 0.74 )
+
+    local transferButton = TDLib( "DButton", bankingFrame )
+    transferButton:ClearPaint()
+    transferButton:Background( Color( 255, 255, 255, 0 ) )
+    transferButton:Outline( Color( 255, 255, 255 ), 2 )
+    transferButton:CircleHover( Color( 255, 255, 255, 25 ) )
+    transferButton:SetText( "Transfer" )
+    transferButton:SetFont( "GBRoboto48" )
+    transferButton:SetSize( GBScaleUI( bankingFrame:GetWide() - 72 ), GBScaleUI( 72 ) )
+    transferButton:SetTextColor( Color( 255, 255, 255 ) )
+    transferButton:CenterHorizontal()
+    transferButton:CenterVertical( 0.9 )
 end
 
 concommand.Add( "gbpanel", function() GlorifiedBanking.OpenPanel() end )
