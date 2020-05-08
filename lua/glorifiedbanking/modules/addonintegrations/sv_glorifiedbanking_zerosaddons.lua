0d10a24b5855d12455e49d9befdc1feb84a012f7
GlorifiedPig <killingpigs123@gmail.com>
2020-05-08 10:14:19 +0200

    support for zero's addons

---
+++ lua/glorifiedbanking/modules/addonintegrations/sv_glorifiedbanking_zerosaddons.lua
@@ -0,0 +1,34 @@
+if not GlorifiedBanking.Config.SUPPORT_ZEROS_ADDONS then return end
+
+if zwf and zwf.f and zwf.f.GiveMoney then
+    local oldzwffunc = zwf.f.GiveMoney
+    function zwf.f.GiveMoney( ply, money )
+        if money >= 0 then
+            GlorifiedBanking.AddPlayerBalance( ply, money )
+        else
+            oldzwffunc( ply, money )
+        end
+    end
+end
+
+if zrmine and zrmine.f and zrmine.f.GiveMoney then
+    local oldzrminefunc = zrmine.f.GiveMoney
+    function zrmine.f.GiveMoney( ply, money )
+        if money >= 0 then
+            GlorifiedBanking.AddPlayerBalance( ply, money )
+        else
+            oldzrminefunc( ply, money )
+        end
+    end
+end
+
+if ztm and ztm.f and ztm.f.GiveMoney then
+    local oldztmfunc = ztm.f.GiveMoney
+    function ztm.f.GiveMoney( ply, money )
+        if money >= 0 then
+            GlorifiedBanking.AddPlayerBalance( ply, money )
+        else
+            oldztmfunc( ply, money )
+        end
+    end
+end
--- lua/glorifiedbanking/sv_config.lua
+++ lua/glorifiedbanking/sv_config.lua
@@ -1,21 +1,31 @@
 
-GlorifiedBanking.Config.SQL_TYPE = "mysqloo" -- 'sqlite' or 'mysqloo'
-GlorifiedBanking.Config.SQL_DETAILS = {
-    [ "host" ] = "localhost",
-    [ "user" ] = "root",
-    [ "pass" ] = "",
-    [ "database" ] = "glorifiedbanking",
-    [ "port" ] = 3306
-}
-GlorifiedBanking.Config.STARTING_BALANCE = 5000 -- How much money to start with in the player's bank account?
+--[[ SQL Configuration ]]--
+    GlorifiedBanking.Config.SQL_TYPE = "mysqloo" -- 'sqlite' or 'mysqloo'
+    GlorifiedBanking.Config.SQL_DETAILS = {
+        [ "host" ] = "localhost",
+        [ "user" ] = "root",
+        [ "pass" ] = "",
+        [ "database" ] = "glorifiedbanking",
+        [ "port" ] = 3306
+    }
+--[[ End SQL Configuration ]]--
 
-GlorifiedBanking.Config.INTEREST_ENABLED = true -- Should interest be enabled or not?
-GlorifiedBanking.Config.INTEREST_TIMER = 20 -- How often should the player receive interest?
-GlorifiedBanking.Config.INTEREST_MAX = 50000 -- What's the maximum amount a player can receive in interest?
-GlorifiedBanking.Config.DEFAULT_INTEREST_PERCENTAGE = 0.1 -- What % should the player get for interest per x seconds?
-GlorifiedBanking.Config.USERGROUP_SPECIFIC_INTERESTS = { -- What % should certain usergroups receive for interest?
-    ["donator"] = 0.2,
-    ["superadmin"] = 1
-}
+--[[ Interest Settings ]]--
+    GlorifiedBanking.Config.INTEREST_ENABLED = true -- Should interest be enabled or not?
+    GlorifiedBanking.Config.INTEREST_TIMER = 20 -- How often should the player receive interest?
+    GlorifiedBanking.Config.INTEREST_MAX = 50000 -- What's the maximum amount a player can receive in interest?
+    GlorifiedBanking.Config.DEFAULT_INTEREST_PERCENTAGE = 0.1 -- What % should the player get for interest per x seconds?
+    GlorifiedBanking.Config.USERGROUP_SPECIFIC_INTERESTS = { -- What % should certain usergroups receive for interest?
+        ["donator"] = 0.2,
+        ["superadmin"] = 1
+    }
+--[[ End Interest Settings ]]--
 
-GlorifiedBanking.Config.SALARY_TO_BANK = true -- Should the player's salary get transferred to his bank account?
+--[[ Addon Integrations ]]--
+    GlorifiedBanking.Config.SUPPORT_ZEROS_ADDONS = true -- If you have any of Zero's addons on your server, you can choose for your income to go to your bank with this option.
+--[[ End Addon Integrations ]]--
+
+--[[ Other Options ]]--
+    GlorifiedBanking.Config.SALARY_TO_BANK = true -- Should the player's salary get transferred to his bank account?
+    GlorifiedBanking.Config.STARTING_BALANCE = 5000 -- How much money to start with in the player's bank account?
+--[[ End Other Options ]]--
