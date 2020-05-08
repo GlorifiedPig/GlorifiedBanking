e96295e5194c10f9b96463bd49972a8c08a0ff8a
GlorifiedPig <killingpigs123@gmail.com>
2020-05-08 18:16:03 +0200

    fix blogs integration

--- lua/glorifiedbanking/modules/integrations/sv_glorifiedbanking_blogs.lua
+++ lua/glorifiedbanking/modules/integrations/sv_glorifiedbanking_blogs.lua
@@ -9,7 +9,7 @@ WITHDRAWAL_MODULE.Color = Color( 0, 255, 0 )
 
 WITHDRAWAL_MODULE:SETUP( function()
     WITHDRAWAL_MODULE:Hook( "GlorifiedBanking.PlayerWithdrawal", "withdrawal", function( ply, withdrawalAmount )
-        WITHDRAWAL_MODULE:LogPhrase( "player_withdrawal", GAS.Logging:FormatPlayer( ply ), GAS.Logging:Highlight( "Withdrew " .. DarkRP.formatMoney( withdrawalAmount ) ) )
+        WITHDRAWAL_MODULE:Log( "{1} withdrew {2}.", GAS.Logging:FormatPlayer( ply ), GAS.Logging:Highlight( DarkRP.formatMoney( withdrawalAmount ) ) )
     end )
 end )
 
@@ -23,7 +23,7 @@ DEPOSIT_MODULE.Color = Color( 255, 0, 0 )
 
 DEPOSIT_MODULE:SETUP( function()
     DEPOSIT_MODULE:Hook( "GlorifiedBanking.PlayerDeposit", "deposit", function( ply, depositAmount )
-        DEPOSIT_MODULE:LogPhrase( "player_deposited", GAS.Logging:FormatPlayer( ply ), GAS.Logging:Highlight( "Deposited " .. DarkRP.formatMoney( depositAmount ) ) )
+        DEPOSIT_MODULE:Log( "{1} deposited {2}.", GAS.Logging:FormatPlayer( ply ), GAS.Logging:Highlight( DarkRP.formatMoney( depositAmount ) ) )
     end )
 end )
 
@@ -37,7 +37,7 @@ TRANSFER_MODULE.Color = Color( 0, 0, 255 )
 
 TRANSFER_MODULE:SETUP( function()
     TRANSFER_MODULE:Hook( "GlorifiedBanking.PlayerTransfer", "transfer", function( ply, receiver, transferAmount )
-        TRANSFER_MODULE:LogPhrase( "player_transferred", GAS.Logging:FormatPlayer( ply ), GAS.Logging:Highlight( "Transferred " .. DarkRP.formatMoney( transferAmount ) .. " to " .. GAS.Logging:FormatPlayer( receiver ) ) )
+        TRANSFER_MODULE:Log( "{1} transferred {2} to {3}.", GAS.Logging:FormatPlayer( ply ), GAS.Logging:FormatPlayer( receiver ), GAS.Logging:Highlight( DarkRP.formatMoney( transferAmount ) ) )
     end )
 end )
 
