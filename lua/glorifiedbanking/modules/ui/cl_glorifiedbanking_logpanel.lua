357dc44ccc64dde23cc28454f56a8607c30a15d8
GlorifiedPig <killingpigs123@gmail.com>
2020-05-08 15:13:03 +0200

    log menu improvements

--- lua/glorifiedbanking/modules/ui/cl_glorifiedbanking_logpanel.lua
+++ lua/glorifiedbanking/modules/ui/cl_glorifiedbanking_logpanel.lua
@@ -18,6 +18,23 @@ function GlorifiedBanking.OpenLogPanel( logTbl )
     for k, v in pairs( logTbl.Withdrawals ) do
         logWithdrawalsSheet:AddLine( v["Date"], v["SteamID"], v["WithdrawAmount"] )
     end
+    logWithdrawalsSheet.OnRowRightClick = function( lineID, line )
+        local withdrawalMenu = DermaMenu()
+
+        withdrawalMenu:AddOption( "Copy Date", function()
+            SetClipboardText( logWithdrawalsSheet:GetLine( line ):GetColumnText( 1 ) )
+        end ):SetIcon( "icon16/date.png" )
+
+        withdrawalMenu:AddOption( "Copy SteamID", function()
+            SetClipboardText( logWithdrawalsSheet:GetLine( line ):GetColumnText( 2 ) )
+        end ):SetIcon( "icon16/page_white_text_width.png" )
+
+        withdrawalMenu:AddOption( "Copy Amount", function()
+            SetClipboardText( logWithdrawalsSheet:GetLine( line ):GetColumnText( 3 ) )
+        end ):SetIcon( "icon16/money.png" )
+
+        withdrawalMenu:Open()
+    end
 
     local logDepositsSheet = vgui.Create( "DListView", logPropertySheet )
     logDepositsSheet:Dock( FILL )
@@ -26,6 +43,23 @@ function GlorifiedBanking.OpenLogPanel( logTbl )
     logDepositsSheet:AddColumn( "Deposit Amount" )
     for k, v in pairs( logTbl.Deposits ) do
         logDepositsSheet:AddLine( v["Date"], v["SteamID"], v["DepositAmount"] )
+    end
+    logDepositsSheet.OnRowRightClick = function( lineID, line )
+        local depositMenu = DermaMenu()
+
+        depositMenu:AddOption( "Copy Date", function()
+            SetClipboardText( logDepositsSheet:GetLine( line ):GetColumnText( 1 ) )
+        end ):SetIcon( "icon16/date.png" )
+
+        depositMenu:AddOption( "Copy SteamID", function()
+            SetClipboardText( logDepositsSheet:GetLine( line ):GetColumnText( 2 ) )
+        end ):SetIcon( "icon16/page_white_text_width.png" )
+
+        depositMenu:AddOption( "Copy Amount", function()
+            SetClipboardText( logDepositsSheet:GetLine( line ):GetColumnText( 3 ) )
+        end ):SetIcon( "icon16/money.png" )
+
+        depositMenu:Open()
     end
 
     local logTransfersSheet = vgui.Create( "DListView", logPropertySheet )
@@ -36,6 +70,27 @@ function GlorifiedBanking.OpenLogPanel( logTbl )
     logTransfersSheet:AddColumn( "Transfer Amount" )
     for k, v in pairs( logTbl.Transfers ) do
         logTransfersSheet:AddLine( v["Date"], v["SteamID"], v["ReceiverSteamID"], v["TransferAmount"] )
+    end
+    logTransfersSheet.OnRowRightClick = function( lineID, line )
+        local transferMenu = DermaMenu()
+
+        transferMenu:AddOption( "Copy Date", function()
+            SetClipboardText( logTransfersSheet:GetLine( line ):GetColumnText( 1 ) )
+        end ):SetIcon( "icon16/date.png" )
+
+        transferMenu:AddOption( "Copy SteamID", function()
+            SetClipboardText( logTransfersSheet:GetLine( line ):GetColumnText( 2 ) )
+        end ):SetIcon( "icon16/page_white_text_width.png" )
+
+        transferMenu:AddOption( "Copy Receiver SteamID", function()
+            SetClipboardText( logTransfersSheet:GetLine( line ):GetColumnText( 3 ) )
+        end ):SetIcon( "icon16/page_white_width.png" )
+
+        transferMenu:AddOption( "Copy Amount", function()
+            SetClipboardText( logTransfersSheet:GetLine( line ):GetColumnText( 4 ) )
+        end ):SetIcon( "icon16/money.png" )
+
+        transferMenu:Open()
     end
 
     logPropertySheet:AddSheet( "Withdrawals", logWithdrawalsSheet, "icon16/arrow_in.png" )
@@ -48,6 +103,5 @@ net.Receive( "GlorifiedBanking.PlayerOpenedLogs", function()
     logTbl.Withdrawals = util.JSONToTable( net.ReadLargeString() )
     logTbl.Deposits = util.JSONToTable( net.ReadLargeString() )
     logTbl.Transfers = util.JSONToTable( net.ReadLargeString() )
-    PrintTable(logTbl.Withdrawals)
     GlorifiedBanking.OpenLogPanel( logTbl )
 end )
