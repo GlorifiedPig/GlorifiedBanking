d8fd3954d084281cbbe3c72005a1752818e16ddb
GlorifiedPig <killingpigs123@gmail.com>
2020-05-09 17:19:30 +0200

    Update cl_glorifiedbanking_themes.lua

--- lua/glorifiedbanking/libraries/cl_glorifiedbanking_themes.lua
+++ lua/glorifiedbanking/libraries/cl_glorifiedbanking_themes.lua
@@ -1,8 +1,9 @@
+
 GlorifiedBanking.Themes = {}
 local registeredThemes = {}
-local selectedTheme = cookie.GetString("GlorifiedBanking.Theme", "Dark")
+local selectedTheme = cookie.GetString( "GlorifiedBanking.Theme", "Dark" )
 
-function GlorifiedBanking.Themes.Register(id, name, data)
+function GlorifiedBanking.Themes.Register( id, name, data )
     if not registeredThemes[id] then
         registeredThemes[id] = {}
     end
@@ -11,17 +12,30 @@ function GlorifiedBanking.Themes.Register(id, name, data)
     registeredThemes[id].Data = data
 end
 
-function GlorifiedBanking.Themes.Get(id)
+function GlorifiedBanking.Themes.Get( id )
     return registeredThemes[id] or registeredThemes["Dark"] or false
 end
 
 function GlorifiedBanking.Themes.GetCurrent()
-    return GlorifiedBanking.Themes.Get(selectedTheme)
+    return GlorifiedBanking.Themes.Get( selectedTheme )
 end
 
-function GlorifiedBanking.Themes.Select(id)
+function GlorifiedBanking.Themes.GetAll()
+    return registeredThemes
+end
+
+function GlorifiedBanking.Themes.GetByName( name )
+    local returnedTheme = registeredThemes["Dark"]
+    for k, v in pairs( registeredThemes ) do
+        if v.DisplayName == name then returnedTheme = v break end
+    end
+    return returnedTheme
+end
+
+function GlorifiedBanking.Themes.Select( id )
     if registeredThemes[id] then
-        cookie.Set("GlorifiedBanking.Theme", tostring(id))
-        selectedTheme = tostring(id)
+        cookie.Set( "GlorifiedBanking.Theme", tostring( id ) )
+        selectedTheme = tostring( id )
+        hook.Run( "GlorifiedBanking.ThemeChanged", GlorifiedBanking.Themes.GetCurrent() )
     end
 end
