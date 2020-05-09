4743f7bdc0923a909dce02322bf86eadfa1b49bb
GlorifiedPig <killingpigs123@gmail.com>
2020-05-09 16:58:08 +0200

    Update sv_glorifiedbanking_playermeta.lua

--- lua/glorifiedbanking/modules/core/sv_glorifiedbanking_playermeta.lua
+++ lua/glorifiedbanking/modules/core/sv_glorifiedbanking_playermeta.lua
@@ -5,14 +5,14 @@ end
 
 local function ValidateChecks( ply, balance )
     -- A few validation checks just in case anything slips through.
-    return GlorifiedBanking.LockdownEnabled
+    return not ( GlorifiedBanking.LockdownEnabled
     or not balance
     or balance == nil
     or balance < 0
     or not ply:IsValid()
     or ply:IsBot()
     or not ply:IsFullyAuthenticated()
-    or not ply:IsConnected()
+    or not ply:IsConnected() )
 end
 
 function GlorifiedBanking.SetPlayerBalance( ply, balance )
