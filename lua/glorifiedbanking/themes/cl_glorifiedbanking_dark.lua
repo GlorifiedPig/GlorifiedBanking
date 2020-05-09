cc39604c58e6bae2fdd934fe82b05778b2b86f84
Tom <tomtheawesomest@gmail.com>
2020-05-09 16:36:55 +0100

    Started default (dark) theme

--- lua/glorifiedbanking/themes/cl_glorifiedbanking_dark.lua
+++ lua/glorifiedbanking/themes/cl_glorifiedbanking_dark.lua
@@ -1,1 +1,4 @@
-GlorifiedBanking.Themes.Register("Dark", "Dark", {})
+GlorifiedBanking.Themes.Register("Dark", "Dark", {
+    backgroundColor = Color(69, 69, 69),
+    logoBackgroundColor = Color(46, 162, 212)
+})
