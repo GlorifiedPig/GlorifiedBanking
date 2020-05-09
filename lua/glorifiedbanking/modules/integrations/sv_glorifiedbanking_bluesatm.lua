63ec89fb6786615a1c946633ebdd0f0cb43bf6b7
GlorifiedPig <killingpigs123@gmail.com>
2020-05-09 10:10:01 +0200

    finish blue's ATM data importer

--- lua/glorifiedbanking/modules/integrations/sv_glorifiedbanking_bluesatm.lua
+++ lua/glorifiedbanking/modules/integrations/sv_glorifiedbanking_bluesatm.lua
@@ -12,26 +12,53 @@ function GlorifiedBanking.ImportSteamIDFromBluesATM( steamid, balance )
 end
 
 concommand.Add( "glorifiedbanking_importbluesdata", function( ply )
-    if ply == NULL or ply:IsSuperAdmin() then
-        if BATM then
-            if BATM.Config.UseMySQL == false then
-                if sql.TableExists( "batm_personal_accounts" ) then
-                    local accountDataTbl = sql.Query( "SELECT * FROM `batm_personal_accounts`" )
-                    for k, v in ipairs( accountDataTbl ) do
+    if ply == NULL or ply:IsSuperAdmin() and BATM then
+        if BATM.Config.UseMySQL == false then
+            if sql.TableExists( "batm_personal_accounts" ) then
+                local accountDataTbl = sql.Query( "SELECT * FROM `batm_personal_accounts`" )
+                for k, v in ipairs( accountDataTbl ) do
+                    local steamid = util.SteamIDFrom64( v["steamid"] )
+                    local balance = util.JSONToTable( v["accountinfo"] )["balance"]
+
+                    GlorifiedBanking.ImportSteamIDFromBluesATM( steamid, balance )
+                end
+                print( "[GlorifiedBanking] SQLite data found, conversion success. Please uninstall Blue's ATM and restart your server." )
+            else
+                print( "[GlorifiedBanking] No Blue's ATM data for SQLite found. Conversion failed." )
+            end
+        else
+            require( "mysqloo" )
+            if not mysqloo then print( "[GlorifiedBanking] Failed to load MySQLOO for the Blue's ATM data importer, are you sure it is installed?" ) end
+            local BATMDB = mysqloo.connect( BATM.Config.MySQLDetails.host,
+                BATM.Config.MySQLDetails.username,
+                BATM.Config.MySQLDetails.password,
+                BATM.Config.MySQLDetails.databasename,
+            tonumber( BATM.Config.MySQLDetails.port ) )
+
+            function BATMDB:onConnected()
+                local query = BATMDB:query( "SELECT * FROM `batm_personal_accounts`" )
+
+                function query:onSuccess( queryData )
+                    for k, v in ipairs( queryData ) do
                         local steamid = util.SteamIDFrom64( v["steamid"] )
                         local balance = util.JSONToTable( v["accountinfo"] )["balance"]
 
                         GlorifiedBanking.ImportSteamIDFromBluesATM( steamid, balance )
                     end
-                    print( "[GlorifiedBanking] SQLite data found, conversion success!" )
-                else
-                    print( "[GlorifiedBanking] No Blue's ATM data for SQLite found. Conversion failed." )
+                    print( "[GlorifiedBanking] MySQL data found, conversion success. Please uninstall Blue's ATM and restart your server." )
                 end
-            else
-                -- uses mysql
+
+                function query:onError()
+                    print( "[GlorifiedBanking] Error in MySQL for Blue's ATM data importer. Conversion failed." )
+                end
+
+            query:start()
+                query:wait()
             end
-        else
-            print( "[GlorifiedBanking] Blue's ATM not found on the server. Conversion failed." )
+            BATM:connect()
+            BATM:wait()
         end
+    else
+        print( "[GlorifiedBanking] Blue's ATM not found on the server. Conversion failed." )
     end
 end )
