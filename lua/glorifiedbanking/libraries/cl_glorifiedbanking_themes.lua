ddf5e39d84f5a1e26959beedf5f1f483e9c77073
Tom <tomtheawesomest@gmail.com>
2020-05-10 19:51:53 +0100

    Remove Imgur loader

--- lua/glorifiedbanking/libraries/cl_glorifiedbanking_themes.lua
+++ lua/glorifiedbanking/libraries/cl_glorifiedbanking_themes.lua
@@ -52,27 +52,6 @@ function GlorifiedBanking.Themes.Select( id )
     end
 end
 
-hook.Add( "InitPostEntity", "GlorifiedBanking.Themes.InitPostEntity", function()
-    local tempMat = Material( "nil" )
-
-    for j, u in pairs( registeredThemes ) do
-        for k, v in pairs( u.Data.Materials ) do
-            if not isstring( v ) then continue end
-
-            if file.Exists( folderName .. "/" .. v .. ".png", "DATA" ) then
-                v = Material( "../data/" .. folderName .. "/" .. v .. ".png", "noclamp smooth" )
-                continue
-            end
-
-            v = tempMat
-            http.Fetch( "https://i.imgur.com/" .. v .. ".png", function( body )
-                file.Write( folderName .. "/" .. v .. ".png", body )
-                v = Material( "../data/" .. folderName .. "/" .. v .. ".png", "noclamp smooth" )
-            end)
-        end
-    end
-end )
-
 hook.Add( "OnScreenSizeChanged", "GlorifiedBanking.Themes.OnScreenSizeChanged", function()
     GlorifiedBanking.Themes.GenerateFonts()
 end )
