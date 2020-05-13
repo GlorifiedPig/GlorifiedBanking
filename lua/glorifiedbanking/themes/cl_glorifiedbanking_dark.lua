6d6b698b63a5500fa0f93ef6555833b4392b0770
Tom <tomtheawesomest@gmail.com>
2020-05-13 14:30:39 +0100

    Change comment style to --

--- lua/glorifiedbanking/themes/cl_glorifiedbanking_dark.lua
+++ lua/glorifiedbanking/themes/cl_glorifiedbanking_dark.lua
@@ -163,4 +163,4 @@ GlorifiedBanking.Themes.Register( "Dark", i18n.GetPhrase("gbDarkTheme"), {
     }
 } )
 
-GlorifiedBanking.Themes.GenerateFonts() //Regenerate fonts on theme save for fast development, remove on release
+GlorifiedBanking.Themes.GenerateFonts() --Regenerate fonts on theme save for fast development, remove on release
