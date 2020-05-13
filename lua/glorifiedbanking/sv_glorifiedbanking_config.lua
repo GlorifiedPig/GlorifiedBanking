839142c27f743454db6d69e77d40955c66e6a9ae
GlorifiedPig <killingpigs123@gmail.com>
2020-05-13 15:12:11 +0200

    network entity + extra validation check

--- lua/glorifiedbanking/sv_glorifiedbanking_config.lua
+++ lua/glorifiedbanking/sv_glorifiedbanking_config.lua
@@ -16,3 +16,28 @@
     GlorifiedBanking.Config.MAX_BACKUPS = 10 -- What are the maximum amount of backups allowed before old ones start getting deleted?
     GlorifiedBanking.Config.BACKUPS_FOLDER_NAME = "glorifiedbanking_backups" -- What should the file name in the data folder be?
 --[[ End Backup System Configuration ]]--
+
+--[[ Other Options ]]--
+    GlorifiedBanking.Config.SALARY_TO_BANK = true -- Should the player's salary get transferred to his bank account?
+    GlorifiedBanking.Config.STARTING_BALANCE = 5000 -- How much money to start with in the player's bank account?
+    GlorifiedBanking.Config.DROP_MONEY_ON_DEATH = false -- Whether or not money should be dropped on death.
+    GlorifiedBanking.Config.DROP_MONEY_ON_DEATH_AMOUNT = 500 -- If the above is set to true, how much should the player drop on death?
+    GlorifiedBanking.Config.MAXIMUM_DISTANCE_FROM_ATM = 750 -- What is the maximum distance for the validation checks on the ATMs?
+--[[ End Other Options ]]--
+
+--[[ Permissions Settings ]]--
+    GlorifiedBanking.Config.CAMI_PERMISSION_DEFAULTS = {
+        ["glorifiedbanking_openadminpanel"] = {
+            MinAccess = "admin",
+            Description = "Determines whether or not the player can open the GlorifiedBanking admin panel."
+        },
+        ["glorifiedbanking_togglelockdown"] = {
+            MinAccess = "admin",
+            Description = "Permission for which usergroups are able to enable/disable lockdown mode."
+        },
+        ["glorifiedbanking_restorebackup"] = {
+            MinAccess = "superadmin",
+            Description = "Permission for which usergroups are able to restore to a previous backup."
+        }
+    }
+--[[ End Permissions Settings ]]--
