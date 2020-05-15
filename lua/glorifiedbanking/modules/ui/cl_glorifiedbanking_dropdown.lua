f4540104d76c6823977bc4e135dbf46fdaa007bf
Tom <tomtheawesomest@gmail.com>
2020-05-15 18:34:10 +0100

    Add hover colours to dropdowns

--- lua/glorifiedbanking/modules/ui/cl_glorifiedbanking_dropdown.lua
+++ lua/glorifiedbanking/modules/ui/cl_glorifiedbanking_dropdown.lua
@@ -8,12 +8,25 @@ function PANEL:Init()
 
     self:SetFont("GlorifiedBanking.AdminMenu.Dropdown")
     self:SetColor(self.Theme.Data.Colors.dropdownSelectedTextCol)
+
+    self.BackgroundColour = Color(0, 0, 0)
+end
+
+local lerp = Lerp
+local function lerpColor(t, from, to)
+    local col = Color(0, 0, 0)
+
+    col.r = lerp(t, from.r, to.r)
+    col.g = lerp(t, from.g, to.g)
+    col.b =  lerp(t, from.b, to.b)
+
+    return col
 end
 
 function PANEL:Paint(w, h)
-    surface.SetFont("GlorifiedBanking.AdminMenu.Dropdown")
+    self.BackgroundColour = lerpColor(FrameTime() * 10, self.BackgroundColour, self:IsHovered() and self.Theme.Data.Colors.dropdownBackgroundHoverCol or self.Theme.Data.Colors.dropdownBackgroundCol)
 
-    draw.RoundedBox(h * .2, 0, 0, w, h, self.Theme.Data.Colors.dropdownBackgroundCol)
+    draw.RoundedBox(h * .2, 0, 0, w, h, self.BackgroundColour)
 end
 
 vgui.Register("GlorifiedBanking.Dropdown", PANEL, "DComboBox")
