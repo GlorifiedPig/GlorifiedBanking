1111e37e1bf682acabc0e03ad0cc0540a1b92fd7
Tom <tomtheawesomest@gmail.com>
2020-05-18 18:51:40 +0100

    Sort transaction logs by date

--- lua/glorifiedbanking/modules/core/sv_glorifiedbanking_net.lua
+++ lua/glorifiedbanking/modules/core/sv_glorifiedbanking_net.lua
@@ -113,7 +113,7 @@ net.Receive( "GlorifiedBanking.ChangeScreen", function( len, ply )
         atmEntity.LastAction = CurTime()
 
         if newScreen == 7 then -- Is this screen the transaction history screen?
-            GlorifiedBanking.SQL.Query( "SELECT * FROM `gb_logs` WHERE `SteamID` = '" .. ply:SteamID() .. "' OR `ReceiverSteamID` = '" .. ply:SteamID() .. "' LIMIT 10", function( queryResult )
+            GlorifiedBanking.SQL.Query( "SELECT * FROM `gb_logs` WHERE `SteamID` = '" .. ply:SteamID() .. "' OR `ReceiverSteamID` = '" .. ply:SteamID() .. "'ORDER BY `Date` DESC LIMIT 10", function( queryResult )
                 net.Start( "GlorifiedBanking.ChangeScreen.SendLogs" )
                 net.WriteEntity( atmEntity )
                 net.WriteLargeString( util.TableToJSON( queryResult ) )
