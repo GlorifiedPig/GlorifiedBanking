f9def1e1ed726c8ca03fc6ae4b9d1b50563a2324
Tom <tomtheawesomest@gmail.com>
2020-05-19 12:52:26 +0100

    Fix resource.AddWorkshop error

---
+++ lua/autorun/sh_glorifiedbanking_resources.lua
@@ -0,0 +1,97 @@
+
+if SERVER then
+    AddCSLuaFile()
+    resource.AddWorkshop( "2101502704" )
+end
+
+local soundLevel = 58
+
+sound.Add({
+    name = "GlorifiedBanking.Key_Press",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = soundLevel,
+    pitch = 100,
+    sound = "glorified_banking/key_press.mp3"
+})
+
+sound.Add({
+    name = "GlorifiedBanking.Beep_Normal",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = soundLevel,
+    pitch = 100,
+    sound = "glorified_banking/beep_normal.mp3"
+})
+
+sound.Add({
+    name = "GlorifiedBanking.Beep_Attention",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = soundLevel,
+    pitch = 100,
+    sound = "glorified_banking/beep_attention.mp3"
+})
+
+sound.Add({
+    name = "GlorifiedBanking.Beep_Error",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = soundLevel,
+    pitch = 100,
+    sound = "glorified_banking/beep_error.mp3"
+})
+
+sound.Add({
+    name = "GlorifiedBanking.Card_Insert",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = soundLevel,
+    pitch = 100,
+    sound = "glorified_banking/card_insert.mp3"
+})
+
+sound.Add({
+    name = "GlorifiedBanking.Card_Remove",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = soundLevel,
+    pitch = 100,
+    sound = "glorified_banking/card_remove.mp3"
+})
+
+sound.Add({
+    name = "GlorifiedBanking.Money_In_Start",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = soundLevel,
+    pitch = 100,
+    sound = "glorified_banking/money_in_start.mp3"
+})
+
+sound.Add({
+    name = "GlorifiedBanking.Money_In_Loop",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = soundLevel,
+    pitch = 100,
+    sound = "glorified_banking/money_in_loop.wav"
+})
+
+sound.Add({
+    name = "GlorifiedBanking.Money_In_Finish",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = soundLevel,
+    pitch = 100,
+    sound = "glorified_banking/money_in_finish.mp3"
+})
+
+sound.Add({
+    name = "GlorifiedBanking.Money_Out",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = soundLevel,
+    pitch = 100,
+    sound = "glorified_banking/money_out.mp3"
+})
