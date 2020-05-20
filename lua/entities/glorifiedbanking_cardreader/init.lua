310656e5549b840ce10191a98ff23342137848d0
Tom <tomtheawesomest@gmail.com>
2020-05-20 12:05:59 +0100

    Transaction method

--- lua/entities/glorifiedbanking_cardreader/init.lua
+++ lua/entities/glorifiedbanking_cardreader/init.lua
@@ -28,9 +28,28 @@ end
 --Transer money method, the main function of the card reader
 function ENT:Transfer(sender)
     local merchant = self:GetMerchant()
+    if not IsValid(merchant) then return end
 
-    GlorifiedBanking.Notify(sender, NOTIFY_GENERIC, 5, i18n.GetPhrase("gbPaidByCard", receiver:Name(), GlorifiedBanking.FormatMoney(amount)))
-    GlorifiedBanking.Notify(merchant, NOTIFY_GENERIC, 5, i18n.GetPhrase("gbPaidByCardReceive", sender:Name(), GlorifiedBanking.FormatMoney(amount)))
+    local amount = self:GetTransactionAmount(0)
+    if amount <= 0 then
+        GlorifiedBanking.Notify(sender, NOTIFY_ERROR, 5, i18n.GetPhrase("gbInvalidAmount"))
+        return
+    end
 
-    GlorifiedBanking.TransferAmount(sender, merchant, amount)
+    local fee = math.Clamp(math.floor(amount / 100 * GlorifiedBanking.Config.CARD_PAYMENT_FEE), 0, amount)
+
+    if not GlorifiedBanking.CanPlayerAfford(sender, amount) then
+        GlorifiedBanking.Notify(sender, NOTIFY_ERROR, 5, i18n.GetPhrase( "gbCannotAfford"))
+        return
+    end
+
+    GlorifiedBanking.RemovePlayerBalance(sender, fee)
+
+    timer.Simple(3, function() --Wait for insert card animation
+        timer.Simple(2, function() --Wait while we contact the server
+            GlorifiedBanking.TransferAmount(sender, merchant, amount)
+            GlorifiedBanking.Notify(sender, NOTIFY_GENERIC, 5, i18n.GetPhrase("gbPaidByCard", receiver:Name(), GlorifiedBanking.FormatMoney(amount)))
+            GlorifiedBanking.Notify(merchant, NOTIFY_GENERIC, 5, i18n.GetPhrase("gbPaidByCardReceive", sender:Name(), GlorifiedBanking.FormatMoney(amount)))
+        end)
+    end)
 end
