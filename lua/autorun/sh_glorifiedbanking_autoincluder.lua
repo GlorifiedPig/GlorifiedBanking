049ad3d9badf9c69949ca93282c83fa88b77f22b
GlorifiedPig <killingpigs123@gmail.com>
2020-05-21 22:28:00 +0200

    v2.3.2

--- lua/autorun/sh_glorifiedbanking_autoincluder.lua
+++ lua/autorun/sh_glorifiedbanking_autoincluder.lua
@@ -7,7 +7,7 @@
 
 GlorifiedBanking = GlorifiedBanking or {
     Config = {},
-    Version = "2.3.1"
+    Version = "2.3.2"
 }
 
 print( "[GlorifiedBanking] This server is running version " .. GlorifiedBanking.Version .. "." )
