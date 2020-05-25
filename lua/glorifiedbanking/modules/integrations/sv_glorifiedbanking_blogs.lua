50975558affbfcd5225ab0f9a0befcb3aaccf2bc
GlorifiedPig <killingpigs123@gmail.com>
2020-05-25 15:04:06 +0200

    fix bLogs integration

--- lua/glorifiedbanking/modules/integrations/sv_glorifiedbanking_blogs.lua
+++ lua/glorifiedbanking/modules/integrations/sv_glorifiedbanking_blogs.lua
@@ -5,12 +5,10 @@ local WITHDRAWAL_MODULE = GAS.Logging:MODULE()
 
 WITHDRAWAL_MODULE.Category = "GlorifiedBanking"
 WITHDRAWAL_MODULE.Name = "Withdrawals"
-WITHDRAWAL_MODULE.Color = Color( 0, 255, 0 )
+WITHDRAWAL_MODULE.Colour = Color( 0, 255, 0 )
 
-WITHDRAWAL_MODULE:SETUP( function()
-    WITHDRAWAL_MODULE:Hook( "GlorifiedBanking.PlayerWithdrawal", "withdrawal", function( ply, withdrawalAmount )
-        WITHDRAWAL_MODULE:Log( "{1} withdrew {2}.", GAS.Logging:FormatPlayer( ply ), GAS.Logging:Highlight( GlorifiedBanking.FormatMoney( withdrawalAmount ) ) )
-    end )
+WITHDRAWAL_MODULE:Hook( "GlorifiedBanking.PlayerWithdrawal", "withdrawal", function( ply, withdrawalAmount )
+    WITHDRAWAL_MODULE:Log( "{1} withdrew {2}.", GAS.Logging:FormatPlayer( ply ), GAS.Logging:Highlight( GlorifiedBanking.FormatMoney( withdrawalAmount ) ) )
 end )
 
 GAS.Logging:AddModule( WITHDRAWAL_MODULE )
@@ -19,12 +17,10 @@ local DEPOSIT_MODULE = GAS.Logging:MODULE()
 
 DEPOSIT_MODULE.Category = "GlorifiedBanking"
 DEPOSIT_MODULE.Name = "Deposits"
-DEPOSIT_MODULE.Color = Color( 255, 0, 0 )
+DEPOSIT_MODULE.Colour = Color( 255, 0, 0 )
 
-DEPOSIT_MODULE:SETUP( function()
-    DEPOSIT_MODULE:Hook( "GlorifiedBanking.PlayerDeposit", "deposit", function( ply, depositAmount )
-        DEPOSIT_MODULE:Log( "{1} deposited {2}.", GAS.Logging:FormatPlayer( ply ), GAS.Logging:Highlight( GlorifiedBanking.FormatMoney( depositAmount ) ) )
-    end )
+DEPOSIT_MODULE:Hook( "GlorifiedBanking.PlayerDeposit", "deposit", function( ply, depositAmount )
+    DEPOSIT_MODULE:Log( "{1} deposited {2}.", GAS.Logging:FormatPlayer( ply ), GAS.Logging:Highlight( GlorifiedBanking.FormatMoney( depositAmount ) ) )
 end )
 
 GAS.Logging:AddModule( DEPOSIT_MODULE )
@@ -33,12 +29,10 @@ local TRANSFER_MODULE = GAS.Logging:MODULE()
 
 TRANSFER_MODULE.Category = "GlorifiedBanking"
 TRANSFER_MODULE.Name = "Transfers"
-TRANSFER_MODULE.Color = Color( 0, 0, 255 )
+TRANSFER_MODULE.Colour = Color( 0, 0, 255 )
 
-TRANSFER_MODULE:SETUP( function()
-    TRANSFER_MODULE:Hook( "GlorifiedBanking.PlayerTransfer", "transfer", function( ply, receiver, transferAmount )
-        TRANSFER_MODULE:Log( "{1} transferred {2} to {3}.", GAS.Logging:FormatPlayer( ply ), GAS.Logging:FormatPlayer( receiver ), GAS.Logging:Highlight( GlorifiedBanking.FormatMoney( transferAmount ) ) )
-    end )
+TRANSFER_MODULE:Hook( "GlorifiedBanking.PlayerTransfer", "transfer", function( ply, receiver, transferAmount )
+    TRANSFER_MODULE:Log( "{1} transferred {2} to {3}.", GAS.Logging:FormatPlayer( ply ), GAS.Logging:FormatPlayer( receiver ), GAS.Logging:Highlight( GlorifiedBanking.FormatMoney( transferAmount ) ) )
 end )
 
 GAS.Logging:AddModule( TRANSFER_MODULE )
