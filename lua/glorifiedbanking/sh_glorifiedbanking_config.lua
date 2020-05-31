4a27eca3ea079abde7737685046383c3be94adb0
GlorifiedPig <killingpigs123@gmail.com>
2020-05-31 16:49:03 +0200

    default config changes

--- lua/glorifiedbanking/sh_glorifiedbanking_config.lua
+++ lua/glorifiedbanking/sh_glorifiedbanking_config.lua
@@ -2,12 +2,12 @@
 --[[ Addon Integrations ]]--
     GlorifiedBanking.Config.SUPPORT_ZEROS_ADDONS = false -- If you have any of Zero's addons on your server, you can choose for your income to go to your bank with this option.
     GlorifiedBanking.Config.SUPPORT_BLOGS = false -- Set this to true if you'd like to log withdrawals, deposits and transfers via bLogs.
-    GlorifiedBanking.Config.SUPPORT_GSMARTWATCH = false -- Set this to true if you'd like to allow payment by smart watch.
+    GlorifiedBanking.Config.SUPPORT_GSMARTWATCH = false -- Set this to true if you'd like to allow payment via smart watch.
 --[[ End Addon Integrations ]]--
 
 --[[ Interest Settings ]]--
     GlorifiedBanking.Config.INTEREST_ENABLED = true -- Should interest be enabled or not?
-    GlorifiedBanking.Config.INTEREST_TIMER = 120 -- How often should the player receive interest?
+    GlorifiedBanking.Config.INTEREST_TIMER = 120 -- How often should the player receive interest? This amount is in seconds.
     GlorifiedBanking.Config.INTEREST_MAX = 10000 -- What's the maximum amount a player can receive in interest?
     GlorifiedBanking.Config.DEFAULT_INTEREST_PERCENTAGE = 0.1 -- What % should the player get for interest per x seconds? Set to 0 to disable interest for normal players.
     GlorifiedBanking.Config.INTEREST_AMOUNT_CUSTOMFUNC = function( ply ) -- Special function to return different interest for certain players.
--- lua/glorifiedbanking/sv_glorifiedbanking_config.lua
+++ lua/glorifiedbanking/sv_glorifiedbanking_config.lua
@@ -12,7 +12,7 @@
 
 --[[ Backup System Configuration ]]--
     GlorifiedBanking.Config.BACKUPS_ENABLED = true -- Should the backup system be enabled?
-    GlorifiedBanking.Config.BACKUP_FREQUENCY = 1 -- How often should backups occur in hours?
+    GlorifiedBanking.Config.BACKUP_FREQUENCY = 12 -- How often should backups occur in hours?
     GlorifiedBanking.Config.MAX_BACKUPS = 10 -- What are the maximum amount of backups allowed before old ones start getting deleted?
     GlorifiedBanking.Config.BACKUPS_FOLDER_NAME = "glorifiedbanking_backups" -- What should the file name in the data folder be?
 --[[ End Backup System Configuration ]]--
